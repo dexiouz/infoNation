8cd23e3304df70749ebf6163b9d9d9519d66fb1f
chidera <dexiouz@gmail.com>
2019-03-23 18:28:12 +0100

    created about page

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout';
+const About = () => (
+  <Layout>
+    <div>
+      <h1> Next App to view Information about any country</h1>
+    </div>
+  </Layout>
+);
+export default About;
