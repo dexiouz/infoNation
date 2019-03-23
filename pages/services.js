5b4794b380e7a264a3248c47619f72ba3398a4f8
chidera <dexiouz@gmail.com>
2019-03-23 18:28:22 +0100

    created services page

---
+++ pages/services.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout';
+const Services = () => (
+  <Layout>
+    <div>
+      <h1> Our services include providing country information from a REST API</h1>
+    </div>
+  </Layout>
+);
+export default Services;
