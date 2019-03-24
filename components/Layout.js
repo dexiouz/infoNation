226ce00c1ea259df1d3c5d34b4c4e0ffca303d06
chidera <dexiouz@gmail.com>
2019-03-24 23:49:41 +0100

    Completed Layout

--- components/Layout.js
+++ components/Layout.js
@@ -3,6 +3,9 @@ import Head from 'next/head';
 const Layout = (props) => (
   <div>
     <Head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
       <title>InfoNation</title>
       <link rel="stylesheet" href="bootstrap.com/....." />
     </Head>
